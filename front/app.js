a7f7b9ef36f6fdc8e7e0c2a7c52fd9f3c7d69dc0
SergiiLat <msv2003@ukr.net>
2016-12-09 18:31:54 +0200

    add remove basket

--- front/app.js
+++ front/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 var token = $.cookie("token")
+var basket;
 
   if (token) {
     $('#userInfo').removeClass('hidden')
@@ -46,14 +47,30 @@ var token = $.cookie("token")
       $.ajax({
       type: 'POST',
       data: {
-        productId: $('.product-buy').parents('.product').attr('id').replace('product-', ''),
+        productId: $(this).attr('product_id')//$('.product-buy').parents('.product').attr('id').replace('product-', ''),
       },
       url: '/addToBasket',
       success: function (msg) {
         console.log(msg)
+        getBasket();
       }})
 
   })
+
+  $('body').on('click',".product-remove", function(e) {
+    $.ajax({
+      type: 'POST',
+      data: {
+        productId: $(this).attr('basket_id')
+      },
+      url: '/removeFromBasket',
+      success: function (msg) {
+        console.log(msg)
+        getBasket();
+      }})
+
+  })
+
 
   var getItems = function(){
     $.ajax({
@@ -65,11 +82,12 @@ var token = $.cookie("token")
       url: "/allItems",
       success: function(products) {
         $('#products').html(
+
           products.map(function(product) {
             return '<div class="product" id="product-' + product.id + '">'
               + '<span class="product-name">'+product.name+'</span>'
               + '<span class="product-price">'+product.price+'</span>'
-              + '<button class="product-buy">Add to cart</button>'
+              + '<button class="product-buy" product_id="'+product.id+'">Add to cart</button>'
               + '</div>'
           }).join('\n')
         )
@@ -77,8 +95,34 @@ var token = $.cookie("token")
     });
   }
 
-  getItems();
+  var getBasket = function(){
+    if (token)
+    {
+      $.get('/showBasket',function(response){
+        console.log(response);
+        if (!response.error)
+        {
+          $('#basketItems').html(
+          response.data.map(function(basketItem) {
+            return '<div class="basket-product" id="basket-product-' + basketItem.item_id + '">'
+              + '<span class="basket-product-name">'+basketItem.name+'</span>'
+              + '<span class="basket-product-price">'+basketItem.price+'</span>'
+              + '<button class="product-remove" basket_id="'+basketItem.id+'">Remove</button>'
+              + '</div>'
+          }).join('\n')
+          );
 
+        }
+
+      })
+    }
+  }
+
+
+
+
+  getItems();
+  getBasket();
 
 
 });
--- models/basketModel.js
+++ models/basketModel.js
@@ -18,7 +18,7 @@ module.exports = {
     models.Backet.destroy({
       where: {
         user_id:userId,
-        item_id:itemId
+        id:itemId
       }
     })
       .then(function () {
@@ -30,9 +30,13 @@ module.exports = {
 
   },
   getAll:function(userId,callback){
-    models.Backet.findAll({where:{user_id:userId}})
+    models.Backet.qu
+
+
+
+    models.sequelize.query("select basket.id,basket.user_id,basket.item_id,items.name,items.price from basket join items  on items.id = basket.item_id WHERE user_id="+userId)
       .then(function (items) {
-        callback(null, items)
+        callback(null, items[0])
       })
       .catch(function (err) {
         callback(err)
